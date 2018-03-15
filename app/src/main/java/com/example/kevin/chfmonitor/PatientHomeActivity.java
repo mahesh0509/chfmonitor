4d95c96c1412ef139a928e3a03a90c8af4587aa8
mahesh0509 <mahesh.mukundan1995@gmail.com>
2018-03-16 00:22:12 +0530

    initial commit

--- app/src/main/java/com/example/kevin/chfmonitor/PatientHomeActivity.java
+++ app/src/main/java/com/example/kevin/chfmonitor/PatientHomeActivity.java
@@ -5,12 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.parse.ParseUser;
 
 public class PatientHomeActivity extends AppCompatActivity {
 
-    Button updateprofbtn,plogoutbtn;
+    Button updateprofbtn,plogoutbtn,docbtn,dailybtn,sensorbtn,vitalbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +22,10 @@ public class PatientHomeActivity extends AppCompatActivity {
 
         updateprofbtn= (Button) findViewById(R.id.updateprofID);
         plogoutbtn= (Button) findViewById(R.id.plogoutID);
+        docbtn= (Button) findViewById(R.id.docID);
+        dailybtn= (Button) findViewById(R.id.dailyID);
+        sensorbtn= (Button) findViewById(R.id.sensorID);
+        vitalbtn= (Button) findViewById(R.id.vitalID);
 
         updateprofbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +42,34 @@ public class PatientHomeActivity extends AppCompatActivity {
                 Intent i=new Intent(PatientHomeActivity.this,MainActivity.class);
                 startActivity(i);
                 finish();
+            }
+        });
+
+        docbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PatientHomeActivity.this,"Under Development",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dailybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PatientHomeActivity.this,"Under Development",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sensorbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PatientHomeActivity.this,"Under Development",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        vitalbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PatientHomeActivity.this,"Under Development",Toast.LENGTH_SHORT).show();
             }
         });
 
